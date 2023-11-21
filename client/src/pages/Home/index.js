5bf0b3bacfac8ab509fdc1a0b6bd3d66f0aa3566
Guthikonda Sanath Kumar <madhirepavan@gmail.com>
2023-11-21 14:58:50 +0530

    first commit

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -42,24 +42,39 @@ const Home = () => {
 
         {/* Image and description in a box with a line */}
         <div className="container">
-          <div className="image">
-            <img
-              src="https://img.freepik.com/free-photo/front-view-hardback-books-library_23-2148827223.jpg?w=360&t=st=1700553852~exp=1700554452~hmac=121f9b444a5c964ef0e8cef59143a4c1136adf159b0a78311ff71bb550d6baf1"
-              alt="Library"
-            />
-          </div>
           <div className="text">
-            <h1>
-              Introducing education, the ultimate library management system
-              based in Guntur, TG. Our innovative platform is designed to
-              streamline and optimize library operations, making it easier for
-              educational institutions to manage their resources effectively.
-              With education, librarians can effortlessly organize, track, and
-              access a vast collection of books, digital resources, and other
-              materials, ensuring a seamless experience for both staff and
-              students. Say goodbye to manual processes and embrace the future
-              of library management with education
-            </h1>
+            <h1></h1>
+          </div>
+          <div class="wrapper">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          <div class="banner">
+            <div class="content">
+              <h2>
+                <h1>
+                  Introducing education, the ultimate library management system
+                  based in Guntur, TG. Our innovative platform is designed to
+                  streamline and optimize library operations, making it easier
+                  for educational institutions to manage their resources
+                  effectively. With education, librarians can effortlessly
+                  organize, track, and access a vast collection of books,
+                  digital resources, and other materials, ensuring a seamless
+                  experience for both staff and students. Say goodbye to manual
+                  processes and embrace the future of library management with
+                  education
+                </h1>
+              </h2>
+            </div>
           </div>
         </div>
       </main>
